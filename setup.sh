3cc1ad4fc00da96cda5038c652d6a53517b05fa3
Mike Chester <mike@chester.io>
2023-01-18 19:56:27 -0800

    latest

--- setup.sh
+++ setup.sh
@@ -1,28 +1,19 @@
 #!/bin/bash
 cd ~
-
 SRC=$(pwd)/klipper-config
+PRINTERS="ender iiip"
 
-NAME=iiip
-DST=$(pwd)/${NAME}_data
-rm -rf ${DST}/config
-ln -s ${SRC}/${NAME} ${DST}/config
+for PRINTER in "${PRINTERS}"; do
+    cd ~
 
-cd ${DST}/config
-git clone https://github.com/jschuh/klipper-macros.git
+    DST=$(pwd)/${PRINTER}_data
+    rm -rf ${DST}/config
+    ln -s ${SRC}/${PRINTER} ${DST}/config
 
-# Certs
-ln -s /etc/ssl/snakeoil.pem ${DST}/certs/moonraker.cert
-ln -s /etc/ssl/snakeoil.pem ${DST}/certs/moonraker.key
+    cd ${DST}/config
+    git clone https://github.com/jschuh/klipper-macros.git
 
-NAME=ender
-DST=$(pwd)/${NAME}_data
-rm -rf ${DST}/config
-ln -s ${SRC}/${NAME} ${DST}/config
-
-cd ${DST}/config
-git clone https://github.com/jschuh/klipper-macros.git
-
-# Certs
-ln -s /etc/ssl/snakeoil.pem ${DST}/certs/moonraker.cert
-ln -s /etc/ssl/snakeoil.pem ${DST}/certs/moonraker.key
+    # Certs
+    ln -s /etc/ssl/snakeoil.pem ${DST}/certs/moonraker.cert
+    ln -s /etc/ssl/snakeoil.pem ${DST}/certs/moonraker.key
+done;
