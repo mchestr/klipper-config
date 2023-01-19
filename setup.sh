6c5eca864c2c247c4c9979f8faea9f9007b62617
Mike Chester <mike@chester.io>
2023-01-18 20:09:28 -0800

    latest

--- setup.sh
+++ setup.sh
@@ -3,13 +3,13 @@ SRC=$HOME/klipper-config;
 PRINTERS="ender iiip";
 
 for PRINTER in ${PRINTERS}; do
-    DST=$HOME/${PRINTER}_data;
-    rm -rf ${DST}/config;
-    ln -s ${SRC}/${PRINTER} ${DST}/config;
+    DST="$HOME/${PRINTER}_data";
+    rm -rf "${DST}/config";
+    ln -s "${SRC}/${PRINTER}" "${DST}/config";
 
-    git clone https://github.com/jschuh/klipper-macros.git ${DST}/config/klipper-macros;
+    git clone https://github.com/jschuh/klipper-macros.git "${DST}/config/klipper-macros";
 
     # Certs
-    ln -s /etc/ssl/snakeoil.pem ${DST}/certs/moonraker.cert;
-    ln -s /etc/ssl/snakeoil.pem ${DST}/certs/moonraker.key;
+    ln -s /etc/ssl/snakeoil.pem "${DST}/certs/moonraker.cert";
+    ln -s /etc/ssl/snakeoil.pem "${DST}/certs/moonraker.key";
 done;
